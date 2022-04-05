326e02c630f43974d1b2cf312fa1dc8209061e1d
Lucas Sorensen <lucas.alex.sorensen@gmail.com>
2022-04-05 15:52:45 +0200

    default cuda.FloatTensor

--- sgvop/__main__.py
+++ sgvop/__main__.py
@@ -272,6 +272,5 @@ def train():
 
 
 if __name__ == "__main__":
-    torch.set_default_tensor_type("torch.FloatTensor")
-    # torch.set_default_tensor_type("torch.cuda.FloatTensor")
+    torch.set_default_tensor_type("torch.cuda.FloatTensor")
     train()
